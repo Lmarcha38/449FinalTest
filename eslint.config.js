ee8a4f720c559f6919c7fcb7f53dff5bfc48d45d
Lmarcha38 <117238874+Lmarcha38@users.noreply.github.com>
2025-04-27 21:51:33 +0000

    ignore gone

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx,html}'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
